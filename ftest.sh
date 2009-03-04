39ae7990250799ab1d5b1f73fe680d22be6fbddc
jcarsique <devnull@localhost>
2009-03-04 12:57:34 +0000

    upload succesfully tested archive

--- ftest.sh
+++ ftest.sh
@@ -3,14 +3,15 @@
 # non standard date pattern
 NOW=$(date +"%y%m%d")
 DAILY_RELEASE=${DAILY_RELEASE:-nuxeo-ep-5.2-${NOW}}
+DAILY_DOWNLOAD="zope@gironde.nuxeo.com:/home/zope/static/nuxeo.org/snapshots"
 
 # download and start last packaged server
+rm -f nuxeo-ep*.zip
 wget -r http://selenium.nuxeo.org/hudson/job/Server_Test_5.2_-_Release/lastSuccessfulBuild/artifact/trunk/release/archives/${DAILY_RELEASE}.zip || exit 1
 unzip ${DAILY_RELEASE}.zip
 mkdir output 2>/dev/null
 rm -rf output/jboss 2>/dev/null
 mv ${DAILY_RELEASE} output/jboss
-rm -f nuxeo-ep*.zip
 echo "BINDHOST=127.0.0.1" >output/jboss/bin/bind.conf
 
 # Start jboss
@@ -26,3 +27,7 @@ ret2=$?
 output/jboss/bin/jbossctl stop
 
 [ $ret1 -eq 0 -a $ret2 -eq 0 ] || exit 9
+
+## upload succesfully tested package on http://www.nuxeo.org/static/snapshots/
+scp -C ${DAILY_RELEASE}.zip $DAILY_DOWNLOAD
+rm -f ${DAILY_RELEASE}.zip
