a45ab7bb1a793cafe24f981aee85f9353c73a193
bdelbosc <devnull@localhost>
2009-03-12 15:55:33 +0000

    NXBT-44: Update integration script to use selenium tests from hg.

--- README.txt
+++ README.txt
@@ -2,16 +2,31 @@
 nuxeo integration release
 ================================
 
-Test the release process for a 5.2 under continuous integration.
+Integration builder, and tester.
 
-It requires nx-builder:
-http://svn.nuxeo.org/nuxeo/tools/nx-builder/trunk
+Requirements
+=============
+
+* nx-builder:
+
+  http://svn.nuxeo.org/nuxeo/tools/nx-builder/trunk
+
+
+Integraiont build
+====================
 
 The test.sh script is used by hudson and can be configured::
 
   ADDONS="addon1 addon2" JBOSS_ARCHIVE=/path/to/jboss.zip ./test.sh
 
-It will produces a zip distribution including addons ::
+It will produces a build.tar file that contains
+   the zip distribution including addons and a md5::
 
-   release/archive/nuxeo-ep-090217.zip
+   nuxeo-ep-I20090217_1259.zip
+   nuxeo-ep-I20090217_1259.zip.md5
 
+Testing last successful build and make it available for download
+==================================================================
+
+The ftest.sh script download the latest build.tar available from hudson and
+run selenium tests, if successful it upload the zip files.
--- ftest.sh
+++ ftest.sh
@@ -5,11 +5,12 @@ LAST_BUILD_URL=http://selenium.nuxeo.org/hudson/job/Server_Test_5.2_-_Release/la
 LAST_BUILD=build.tar
 DAILY_DOWNLOAD="zope@gironde.nuxeo.com:/home/zope/static/nuxeo.org/snapshots"
 
-#cleaning
+
+# Cleaning
 rm -rf ./jboss ./results ./download
 mkdir ./results ./download || exit 1
 
-# download and unpack distrib
+# Download and unpack the last build
 cd download
 wget -nv $LAST_BUILD_URL/$LAST_BUILD || exit 1
 tar xvf $LAST_BUILD || exit 1
@@ -18,39 +19,31 @@ cd ..
 build=$(find ./download -maxdepth 1 -name 'nuxeo-ep*'  -type d)
 mv $build ./jboss || exit 1
 
+
+# Update selenium tests
+if [ ! -d nuxeo-distribution ]; then
+    hg clone -r $NXVERSION http://hg.nuxeo.org/nuxeo/nuxeo-distribution 2>/dev/null || exit 1
+else
+    (cd nuxeo-distribution && hg pull && hg up -C $NXVERSION) || exit 1
+fi
+
 # Start jboss
 echo "BINDHOST=0.0.0.0" > jboss/bin/bind.conf
 ./jboss/bin/jbossctl start || exit 1
 
 
 # Run selenium tests
-CMD="xvfb-run -e xvfb.log java -jar selenium/selenium-server.jar -port 14440 -timeout 7200"
-if [ $NXVERSION = "5.1" ] ; then
-        suite1=suite1.html
-        suite2=suite2.html
-else
-        suite1=suite1-5.2.html
-        suite2=suite2-5.2.html
-fi
-
-($CMD -htmlSuite "*firefox" http://localhost:8080/nuxeo/ \
-  "$PWD/selenium/tests/$suite1" "$PWD/results/results1.html" \
-   -userExtensions selenium/user-extensions.js)
+HIDE_FF=true ./nuxeo-platform-ear/ftest/selenium/run.sh
 ret1=$?
-
-($CMD -htmlSuite "*firefox" http://localhost:8080/nuxeo/ \
-  "$PWD/selenium/tests/$suite2" "$PWD/results/results2.html" \
-  -userExtensions selenium/user-extensions.js)
-ret2=$?
-
 
 # Stop nuxeo
 ./jboss/bin/jbossctl stop
 gzip jboss/server/default/log/*.log
 
 # Exit if some tests failed
-[ $ret1 -eq 0 -a $ret2 -eq 0 ] || exit 9
-date
+[ $ret1 -eq 0 ] || exit 9
+
 # Upload succesfully tested package on http://www.nuxeo.org/static/snapshots/
+date
 scp ${build}.zip.md5 ${build}.zip $DAILY_DOWNLOAD || exit 1
 date
