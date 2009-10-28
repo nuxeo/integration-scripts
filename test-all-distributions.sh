8c7da303f3db1e2ba5cb2a459b465bd1721e44d5
bdelbosc <devnull@localhost>
2009-10-28 17:22:38 +0000

    NXBT-92: Run funkload for java 6

--- test-all-distributions.sh
+++ test-all-distributions.sh
@@ -58,9 +58,9 @@ start_jboss
 HIDE_FF=true "$NXDIR"/nuxeo-distribution/nuxeo-distribution-dm/ftest/selenium/run.sh
 ret1=$?
 
-java -version  2>&1 | grep 1.5.0
+java -version  2>&1 | grep 1.6.0
 if [ $? == 0 ]; then
-    # FunkLoad tests works only with java 1.5.0 (j_ids are changed by java6)
+    # FunkLoad tests works only with java 1.6.0 (j_ids are changed by java6)
     (cd "$NXDIR"/nuxeo-distribution/nuxeo-distribution-dm/ftest/funkload; make EXT="--no-color")
     ret2=$?
 else
