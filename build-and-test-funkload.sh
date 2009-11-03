e8296cf6afed0546968872cffc982784061ca77e
bdelbosc <devnull@localhost>
2009-11-03 17:15:48 +0000

    NXBT: build and test script does not require an existing jboss - ter

--- build-and-test-funkload.sh
+++ build-and-test-funkload.sh
@@ -19,7 +19,7 @@ start_jboss
 java -version  2>&1 | grep 1.6.0
 if [ $? == 0 ]; then
     # FunkLoad tests works only with java 1.6.0 (j_ids are changed by java6)
-    (cd "$NXDIR"/nuxeo-distribution/nuxeo-distribution-dm/ftest/funkload; make EXT="--no-color")
+    (cd "$NXDISTRIBUTION"/nuxeo-distribution-dm/ftest/funkload; make EXT="--no-color")
     ret1=$?
 else
     echo "### JAVA 6 required to run funkload tests."
