d3b1729f5b8536923857fe4830eb660fa107d738
jcarsique <devnull@localhost>
2010-05-05 10:38:16 +0000

    restart pound if already running instead of exiting with error

--- cluster-test.sh
+++ cluster-test.sh
@@ -52,6 +52,7 @@ cp -r ./jboss ./jboss2
 
 # Start --------------------------------------------------
 # start pound
+/usr/sbin/pound -f ./pound.cfg -p ./pound.pid || kill `cat ./pound.pid` && \
 /usr/sbin/pound -f ./pound.cfg -p ./pound.pid || exit 1
 
 # Start two JBoss
