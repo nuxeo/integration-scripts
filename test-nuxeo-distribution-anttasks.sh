23433434c76179e803af8faa96869562b258cbb3
jcarsique <devnull@localhost>
2010-10-08 16:45:14 +0000

    use same directory name for jboss as in other tests

--- test-nuxeo-distribution-anttasks.sh
+++ test-nuxeo-distribution-anttasks.sh
@@ -2,8 +2,10 @@
 HERE=$(cd $(dirname $0); pwd -P)
 . $HERE/integration-lib.sh
 
-unzip nuxeo-distribution-jboss/target/nuxeo-distribution-jboss-5.4.0-SNAPSHOT-nuxeo-dm.zip -d jboss
-ant copy -Djboss.dir=jboss/nuxeo-dm-5.4.0-SNAPSHOT-jboss/
+rm -rf jboss
+unzip nuxeo-distribution-jboss/target/nuxeo-distribution-jboss-5.4.0-SNAPSHOT-nuxeo-dm.zip -d /tmp/
+mv /tmp/nuxeo-dm-5.4.0-SNAPSHOT-jboss jboss
+ant copy -Djboss.dir=jboss
 start_jboss
 sleep 3
 stop_jboss
