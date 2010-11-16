b8a0a239a9ae0fdc426152cfa253d35685ce07e0
jcarsique <devnull@localhost>
2010-11-16 19:30:34 +0000

    increased permgen size to 384m

--- nightly-mvn-deploy.sh
+++ nightly-mvn-deploy.sh
@@ -7,7 +7,7 @@ HERE=$(cd $(dirname $0); pwd -P)
 DWS="$HERE"/src
 NXP=${NXP:-5.4}
 NXC=${NXC:-5.4}
-MAVEN_OPTS="-Xmx2048m -Xms512m -XX:MaxPermSize=256m"
+MAVEN_OPTS="-Xmx2048m -Xms512m -XX:MaxPermSize=384m"
 export MAVEN_OPTS
 
 if [ ! -e $DWS ]; then
