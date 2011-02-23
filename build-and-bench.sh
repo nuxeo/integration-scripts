65cc930054279d6e00cc0abb72aeab01c2c6e9e2
bdelbosc <devnull@localhost>
2011-02-23 11:39:45 +0000

    NXBT: Add bench target to build-and-bench script

--- build-and-bench.sh
+++ build-and-bench.sh
@@ -2,6 +2,7 @@
 
 PRODUCT=${PRODUCT:-dm}
 SERVER=${SERVER:-jboss}
+BENCH_TARGET=${BENCH_TARGET:-bench-long}
 HERE=$(cd $(dirname $0); pwd -P)
 . $HERE/integration-lib.sh
 
@@ -33,7 +34,7 @@ start_server 127.0.0.1
 # Run the bench
 echo "Benching nuxeo ep ..."
 test_path="$NXDISTRIBUTION"/nuxeo-distribution-dm/ftest/funkload/
-(cd $test_path; make bench-long EXT="--no-color"; ret=$?; make stop; exit $ret)
+(cd $test_path; make $BENCH_TARGET EXT="--no-color"; ret=$?; make stop; exit $ret)
 ret1=$?
 
 # Move the bench report
