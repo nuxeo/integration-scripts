43810c4bc00a9cabcef0cf6c7973d55a47e77d84
bdelbosc <devnull@localhost>
2011-02-28 09:50:01 +0000

    Add a script to run a bench on the same instance.

---
+++ rebench.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+PRODUCT=${PRODUCT:-dm}
+SERVER=${SERVER:-jboss}
+BENCH_TARGET=${BENCH_TARGET:-bench-reader}
+HERE=$(cd $(dirname $0); pwd -P)
+. $HERE/integration-lib.sh
+
+# Cleaning
+rm -rf ./report ./results 
+mkdir ./results  || exit 1
+
+# Build
+update_distribution_source
+
+# Start Nuxeo
+start_server 127.0.0.1
+
+# Run the bench
+echo "Benching nuxeo ep ..."
+test_path="$NXDISTRIBUTION"/nuxeo-distribution-dm/ftest/funkload/
+(cd $test_path; make $BENCH_TARGET EXT="--no-color"; ret=$?; make stop; exit $ret)
+ret1=$?
+
+# Move the bench report
+mv "$NXDISTRIBUTION"/nuxeo-distribution-dm/target/ftest/funkload/report .
+
+# Stop nuxeo
+stop_server
+
+# Exit if some tests failed
+[ $ret1 -eq 0 ] || exit 9
