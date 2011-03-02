6ddc1819de2d2046436c24341d06baa524fd317e
bdelbosc <devnull@localhost>
2011-03-02 08:57:07 +0000

    Fix build number link

--- build-funkload-trend-report.py
+++ build-funkload-trend-report.py
@@ -106,7 +106,7 @@ for build in builds:
                 label = description # | truncate()
                 metadata.append('label: ' + label)
                 metadata.append(description)
-            metadata.append("build `%s <%s>`__" % (builtOn, BASE_URL + '/' + number))
+            metadata.append("build: `%s <%s>`__" % (number, BASE_URL + '/' + number))
             metadata.append("builtOn: " + builtOn)
             metadata.append('`bench report <' + BASE_URL + '/' + number + '/artifact/trunk/report/' + os.path.basename(report) + '/index.html#page-stats>`__')
             metadata.append('`monitoring <' + BASE_URL + '/' + number + '/artifact/trunk/monitor/monitor.html>`__')
