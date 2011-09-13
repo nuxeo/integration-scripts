035847f5d2cce16840bb3328aaccb452f14d4fd2
bdelbosc <devnull@localhost>
2011-09-13 10:44:53 +0000

    NXBT-318: Add JMeter CMIS test on CI, fix

--- build-and-bench-cmis.sh
+++ build-and-bench-cmis.sh
@@ -19,19 +19,6 @@ DURATION=${DURATION:600}
 rm -rf ./jboss ./report ./results ./download
 mkdir ./results ./download || exit 1
 
-# Build
-update_distribution_source
-if [ "$SERVER" = "tomcat" ]; then
-    build_tomcat
-    NEW_TOMCAT=true
-    setup_tomcat 127.0.0.1
-else
-    build_jboss
-    NEW_JBOSS=true
-    setup_jboss 127.0.0.1
-    deploy_ear
-fi
-
 # Check JMeter
 if [ ! -r $JMETER_HOME/lib/junit ]; then
     echo "ERROR $JMETER_HOME/lib/junit must be writable"
@@ -40,7 +27,7 @@ fi
 
 # Setup JMeter script and deploy JUnit test
 if [ ! -d $CMISBENCH ]; then
-    git clone git@github.com:nuxeo/tools-nuxeo-cmis-jmeter.git $ĈMISBENCH || exit 1
+    git clone git://github.com/nuxeo/tools-nuxeo-cmis-jmeter.git $CMISBENCH || exit 1
 else
     (cd $CMISBENCH &&  git pull) || exit 1
 fi
@@ -60,6 +47,19 @@ EOF
 # Build and deploy JUnit test 
 (cd $CMISBENCH  && ant deploy) || exit 1
 
+
+# Build
+update_distribution_source
+if [ "$SERVER" = "tomcat" ]; then
+    build_tomcat
+    NEW_TOMCAT=true
+    setup_tomcat 127.0.0.1
+else
+    build_jboss
+    NEW_JBOSS=true
+    setup_jboss 127.0.0.1
+    deploy_ear
+fi
 
 # Setup PostgreSQL
 if [ ! -z $PGPASSWORD ]; then
