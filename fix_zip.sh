c1c33232c1f00c9787520c1832b259db92fee88d
Julien Carsique <jcarsique@nuxeo.com>
2012-07-18 18:21:09 +0200

    NXDOC-125 - fix_zip.sh

--- fix_zip.sh
+++ fix_zip.sh
@@ -6,14 +6,14 @@ for zip in $* ; do
     continue
   fi
   echo Fixing $zip ...
-  SOURCE_DIR=$(dirname $zip)
   DIR=$(basename $zip | sed "s/.zip//")
-  rm -rf /tmp/$DIR/
+  rm -rf /tmp/$DIR/ /tmp/$DIR.zip
   unzip $zip -d /tmp/
   cd /tmp/
   chmod +x $DIR/bin/*ctl $DIR/bin/*.sh $DIR/bin/*.command $DIR/*.command
-  zip -r $DIR.zip $DIR/ && mv $DIR.zip $zip
+  zip -r $DIR.zip $DIR/
   rm -rf $DIR/
   cd -
+  mv /tmp/$DIR.zip $zip
   md5sum $zip>$zip.md5
 done
