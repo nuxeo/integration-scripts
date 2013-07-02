e45c119e63f468e09572c3d63b66c30ded2e07e7
Benoit Delbosc <bdelbosc@nuxeo.com>
2013-07-02 17:38:33 +0200

    NXBT-584: try to switch back to XA mode

--- build-and-bench-cmis.sh
+++ build-and-bench-cmis.sh
@@ -66,6 +66,11 @@ if [ ! -z $PGPASSWORD ]; then
     setup_postgresql_database
 fi
 
+# Disable single datasource mode
+cat >> "$NUXEO_CONF" <<EOF || exit 1
+nuxeo.db.singleDataSource=
+EOF
+
 # Start Nuxeo
 start_server 127.0.0.1
 
