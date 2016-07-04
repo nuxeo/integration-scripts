08f867f82798363323b33a864d4d36bee5838dd3
Florent Guillaume <fguillaume@nuxeo.com>
2016-07-04 20:04:30 +0200

    NXBT-1127: fix both server and cap, and include nuxeo-jsf-ui marketplace in server

--- fix_zip.sh
+++ fix_zip.sh
@@ -9,52 +9,82 @@
 #    ├── scripts
 #    └── ...
 
-# Prepare wizard's packages.xml file and fill mp directory
-mkdir archives/mp archives.bak
-unzip -p archives/nuxeo-cap-*-tomcat.zip **/setupWizardDownloads/packages.xml > archives/mp/packages.xml
-featured=`xmlstarlet sel -t -m '//packageDefinitions/package' -i "not(@virtual='true')" -v '@id' -n archives/mp/packages.xml`
-for file in $(grep uploaded nuxeo/marketplace/release.ini |cut -d ' ' -f 4); do
-    # name=`unzip -p $file package.xml|grep -oPm1 '(?<=name=")[^"]+'`
-    name=`unzip -p $file package.xml|xmlstarlet sel -t -v 'package/@name'`
-    if [[ $featured =~ (^|[[:space:]])"$name"($|[[:space:]]) ]]; then
-      cp $file archives/mp/$name.zip
-      xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'filename' -v "$name.zip" archives/mp/packages.xml
-      md5=`md5sum archives/mp/$name.zip |cut -d ' ' -f 1`
-      xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'md5' -v "$md5" archives/mp/packages.xml
-    fi
-done
+# use openssl to compute digests to be portable between Linux and Mac OS X
 
-# Update ZIP archives
+# just the md5 hash
+md5() {
+  openssl dgst -md5 -hex $1 | cut -d ' ' -f 2
+}
+
+# md5 hash followed by two spaces then filename
+md5sum() {
+  echo "$(md5 $1)  $(basename $1)"
+}
+
+# just the sha256 hash
+sha256() {
+  openssl dgst -sha256 -hex $1 | cut -d ' ' -f 2
+}
+
+# sha256 hash followed by two spaces then filename
+sha256sum() {
+  echo "$(sha256 $1)  $(basename $1)"
+}
+
+NUXEO_JSF_UI=nuxeo-jsf-ui
+mkdir -p archives
 cd archives
-for zip in nuxeo-cap-*-tomcat.zip nuxeo-cap-*-tomcat-sdk.zip ; do
-  if [ ! -e $zip ]; then
-    echo Ignore non existing $zip!
-    continue
-  fi
-  echo Fixing $zip ...
-  DIR=$(basename $zip | sed "s/.zip//")
-  cp $zip $(dirname $zip)/$DIR.bak.zip
-  rm -rf /tmp/$DIR/ /tmp/$DIR.zip
-  unzip $zip -d /tmp/
-  cd /tmp/
-  chmod +x $DIR/bin/*ctl $DIR/bin/*.sh $DIR/bin/*.command $DIR/*.command
-  cp $OLDPWD/mp/packages.xml $DIR/setupWizardDownloads/
-  for file in $OLDPWD/mp/*.zip; do
-      md5=`md5sum $file |cut -d ' ' -f 1`
-      cp $file $DIR/setupWizardDownloads/$md5
+for kind in nuxeo-server nuxeo-cap; do
+  MP=mp-$kind
+  mkdir -p $MP
+  PACKAGES_XML=$MP/packages.xml
+  unzip -p $kind-*-tomcat.zip "**/setupWizardDownloads/packages.xml" > $PACKAGES_XML
+  featured=$(xmlstarlet sel -t -m '//packageDefinitions/package' -i "not(@virtual='true')" -v '@id' -n $PACKAGES_XML)
+
+  # Loop on all uploaded marketplace packages and the nuxeo-jsf-ui one prepared for the release
+  for file in $(grep uploaded ../nuxeo/marketplace/release.ini | cut -d ' ' -f 4) $NUXEO_JSF_UI-*.zip; do
+      name=$(unzip -p $file package.xml | xmlstarlet sel -t -v 'package/@name')
+      if [[ $featured =~ (^|[[:space:]])"$name"($|[[:space:]]) ]]; then
+	cp $file $MP/$name.zip
+	xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'filename' -v "$name.zip" $PACKAGES_XML
+	md5=$(md5 $MP/$name.zip)
+	xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'md5' -v "$md5" $PACKAGES_XML
+      fi
   done
-  zip -r $DIR.zip $DIR/
-  rm -rf $DIR/
-  cd -
-  mv /tmp/$DIR.zip $zip
-  md5sum $zip>$zip.md5
-  shasum -a 256 $zip>$zip.sha256
+
+  # Update ZIP archives
+  for zip in $kind-*-tomcat.zip $kind-*-tomcat-sdk.zip ; do
+    if [ ! -e $zip ]; then
+      echo Ignore non existing $zip!
+      continue
+    fi
+    echo Fixing $zip ...
+    DIR=$(basename $zip | sed "s/.zip//")
+    cp $zip $(dirname $zip)/$DIR.bak.zip
+    rm -rf /tmp/$DIR/ /tmp/$DIR.zip
+    unzip $zip -d /tmp/
+    cd /tmp/
+    chmod +x $DIR/bin/*ctl $DIR/bin/*.sh $DIR/bin/*.command $DIR/*.command
+    cp $OLDPWD/$PACKAGES_XML $DIR/setupWizardDownloads/
+    for file in $OLDPWD/$MP/*.zip; do
+	md5=$(md5 $file)
+	cp $file $DIR/setupWizardDownloads/$md5
+    done
+    zip -r $DIR.zip $DIR/
+    rm -rf $DIR/
+    cd -
+    mv /tmp/$DIR.zip $zip
+    md5sum $zip > $zip.md5
+    sha256sum $zip > $zip.sha256
+  done
 done
 
 for file in nuxeo-*-sources.zip; do
     md5sum $file > $file.md5
-    shasum -a 256 $file > $file.sha256
+    sha256sum $file > $file.sha256
 done
 
 cd ..
+mkdir -p archives.bak
 mv archives/*.bak.zip archives.bak/
+mv archives/$NUXEO_JSF_UI-*.zip archives.bak/
