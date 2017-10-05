ca39c8438e379d7dc323b97d8eaa651a2abf168d
Julien Carsique <jcarsique@nuxeo.com>
2017-10-05 19:14:22 +0200

    NXBT-1938: pre indent to ease code review

--- fix_zip.sh
+++ fix_zip.sh
@@ -35,48 +35,48 @@ NUXEO_JSF_UI=nuxeo-jsf-ui
 mkdir -p archives
 cd archives
 for kind in nuxeo-server nuxeo-cap; do
-  MP=mp-$kind
-  mkdir -p $MP
-  PACKAGES_XML=$MP/packages.xml
-  unzip -p $kind-*-tomcat.zip "**/setupWizardDownloads/packages.xml" > $PACKAGES_XML
-  featured=$(xmlstarlet sel -t -m '//packageDefinitions/package' -i "not(@virtual='true')" -v '@id' -n $PACKAGES_XML)
+MP=mp-$kind
+mkdir -p $MP
+PACKAGES_XML=$MP/packages.xml
+unzip -p $kind-*-tomcat.zip "**/setupWizardDownloads/packages.xml" > $PACKAGES_XML
+featured=$(xmlstarlet sel -t -m '//packageDefinitions/package' -i "not(@virtual='true')" -v '@id' -n $PACKAGES_XML)
 
-  # Loop on all uploaded marketplace packages and the nuxeo-jsf-ui one prepared for the release
-  for file in $(grep uploaded ../nuxeo/marketplace/release.ini | cut -d ' ' -f 4-) $NUXEO_JSF_UI-*.zip; do
-    name=$(unzip -p $file package.xml | xmlstarlet sel -t -v 'package/@name') || echo ERROR: package.xml parsing failed on $file >&2
-    if [[ $featured =~ (^|[[:space:]])"$name"($|[[:space:]]) ]]; then
-      cp $file $MP/$name.zip
-      xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'filename' -v "$name.zip" $PACKAGES_XML
-      md5=$(md5 $MP/$name.zip)
-      xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'md5' -v "$md5" $PACKAGES_XML
-    fi
-  done
+# Loop on all uploaded marketplace packages and the nuxeo-jsf-ui one prepared for the release
+for file in $(grep uploaded ../nuxeo/marketplace/release.ini | cut -d ' ' -f 4-) $NUXEO_JSF_UI-*.zip; do
+  name=$(unzip -p $file package.xml | xmlstarlet sel -t -v 'package/@name') || echo ERROR: package.xml parsing failed on $file >&2
+  if [[ $featured =~ (^|[[:space:]])"$name"($|[[:space:]]) ]]; then
+    cp $file $MP/$name.zip
+    xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'filename' -v "$name.zip" $PACKAGES_XML
+    md5=$(md5 $MP/$name.zip)
+    xmlstarlet ed -L -i "//packageDefinitions/package[@id='$name']" -t 'attr' -n 'md5' -v "$md5" $PACKAGES_XML
+  fi
+done
 
-  # Update ZIP archives
-  for zip in $kind-*-tomcat.zip $kind-*-tomcat-sdk.zip ; do
-    if [ ! -e $zip ]; then
-      echo Ignore non existing $zip!
-      continue
-    fi
-    echo Fixing $zip ...
-    DIR=$(basename $zip | sed "s/.zip//")
-    cp $zip $(dirname $zip)/$DIR.bak.zip
-    rm -rf /tmp/$DIR/ /tmp/$DIR.zip
-    unzip $zip -d /tmp/
-    cd /tmp/
-    chmod +x $DIR/bin/*ctl $DIR/bin/*.sh $DIR/bin/*.command $DIR/*.command
-    cp $OLDPWD/$PACKAGES_XML $DIR/setupWizardDownloads/
-    for file in $OLDPWD/$MP/*.zip; do
-  md5=$(md5 $file)
-  cp $file $DIR/setupWizardDownloads/$md5
-    done
-    zip -r $DIR.zip $DIR/
-    rm -rf $DIR/
-    cd -
-    mv /tmp/$DIR.zip $zip
-    md5sum $zip > $zip.md5
-    sha256sum $zip > $zip.sha256
+# Update ZIP archives
+for zip in $kind-*-tomcat.zip $kind-*-tomcat-sdk.zip ; do
+  if [ ! -e $zip ]; then
+    echo Ignore non existing $zip!
+    continue
+  fi
+  echo Fixing $zip ...
+  DIR=$(basename $zip | sed "s/.zip//")
+  cp $zip $(dirname $zip)/$DIR.bak.zip
+  rm -rf /tmp/$DIR/ /tmp/$DIR.zip
+  unzip $zip -d /tmp/
+  cd /tmp/
+  chmod +x $DIR/bin/*ctl $DIR/bin/*.sh $DIR/bin/*.command $DIR/*.command
+  cp $OLDPWD/$PACKAGES_XML $DIR/setupWizardDownloads/
+  for file in $OLDPWD/$MP/*.zip; do
+    md5=$(md5 $file)
+    cp $file $DIR/setupWizardDownloads/$md5
   done
+  zip -r $DIR.zip $DIR/
+  rm -rf $DIR/
+  cd -
+  mv /tmp/$DIR.zip $zip
+  md5sum $zip > $zip.md5
+  sha256sum $zip > $zip.sha256
+done
 done
 
 for file in nuxeo-*-sources.zip; do
