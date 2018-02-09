4ea5019d40a60ff2cbfcc40c5216146aefce0616
alexistimic <atimic@nuxeo.com>
2018-02-09 17:29:03 +0100

    Improvement

--- jenkins_workspace_cleanup.sh
+++ jenkins_workspace_cleanup.sh
@@ -24,7 +24,7 @@
 
 docker image prune -f > /dev/null
 docker volume prune -f > /dev/null
-docker ps -a | awk '/Exited.*(hours)[|](days)[|](weeks) ago/ {print $1}' | xargs --no-run-if-empty docker rm -v > /dev/null 2>&1
+docker ps -a | awk '/Exited.*(hours|days|weeks) ago/ {print $1}' | xargs --no-run-if-empty docker rm -v > /dev/null 2>&1
 find /opt/jenkins/workspace/TestAndPush -maxdepth 1 -mindepth 1 -type d -mtime +3 -exec rm -rf {} +
 find '/opt/jenkins/workspace*/' -name 'nuxeo-server-tomcat-*.zip' -o -name 'nuxeo-cap-*.zip' -mtime +3 -exec rm {} +
-find '/opt/jenkins/workspace*/' -path '*/target/tomcat' -type d -prune -mtime +3 -exec rm -r {} \;
+find '/opt/jenkins/workspace*/' -path '*/target/tomcat' -type d -prune -mtime +3 -exec rm -r {} +
