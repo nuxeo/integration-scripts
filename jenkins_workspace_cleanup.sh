89923b458647200cb9b6e25a7c3ccc3290f5dc3d
alexistimic <atimic@nuxeo.com>
2018-02-09 15:31:26 +0100

    script improvement

--- jenkins_workspace_cleanup.sh
+++ jenkins_workspace_cleanup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # /*
-# (C) Copyright ${year} Nuxeo (http://nuxeo.com/) and others.
+# (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
 # *
 # * Licensed under the Apache License, Version 2.0 (the "License");
 # * you may not use this file except in compliance with the License.
@@ -14,16 +16,15 @@
 # * limitations under the License.
 # *
 # * Contributors:
-# *     ...
+# *     alexis timic
 # */
 
 ###### Cron file for jenkins qa* ######
 
-#!/bin/bash
 
 docker image prune -f > /dev/null
 docker volume prune -f > /dev/null
-docker ps -a | grep Exited | grep "hours ago" | awk '{print $1}' | xargs --no-run-if-empty docker rm -v > /dev/null 2>&1
-find /opt/jenkins/workspace/TestAndPush -maxdepth 1 -mindepth 1 -type d -mtime +3 -exec rm -rf {} \;
-find /opt/jenkins/workspace*/ -name nuxeo-server-tomcat-*.zip -o -name nuxeo-cap-*.zip -mtime +3 -exec rm {} \;
-find /opt/jenkins/workspace* -path \*/target/tomcat -type d -prune -mtime +3 -exec rm -r {} \;
+docker ps -a | awk '/Exited.*(hours)[|](days)[|](weeks) ago/ {print $1}' | xargs --no-run-if-empty docker rm -v > /dev/null 2>&1
+find /opt/jenkins/workspace/TestAndPush -maxdepth 1 -mindepth 1 -type d -mtime +3 -exec rm -rf {} +
+find '/opt/jenkins/workspace*/' -name 'nuxeo-server-tomcat-*.zip' -o -name 'nuxeo-cap-*.zip' -mtime +3 -exec rm {} +
+find '/opt/jenkins/workspace*/' -path '*/target/tomcat' -type d -prune -mtime +3 -exec rm -r {} \;
