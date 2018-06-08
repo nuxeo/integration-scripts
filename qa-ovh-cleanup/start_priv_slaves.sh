943e10407321324afe536ff4ed571e7a096bc0c8
alexistimic <atimic@nuxeo.com>
2018-06-08 14:23:11 +0200

    NXBT-1513: CI CD on docker hosts

--- qa-ovh-cleanup/start_priv_slaves.sh
+++ qa-ovh-cleanup/start_priv_slaves.sh
@@ -1,37 +1,29 @@
-+#!/bin/bash
-+#
-+# (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
-+#
-+# Licensed under the Apache License, Version 2.0 (the "License");
-+# you may not use this file except in compliance with the License.
-+# You may obtain a copy of the License at
-+#
-+#     http://www.apache.org/licenses/LICENSE-2.0
-+#
-+# Unless required by applicable law or agreed to in writing, software
-+# distributed under the License is distributed on an "AS IS" BASIS,
-+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-+# See the License for the specific language governing permissions and
-+# limitations under the License.
-+#
-+# Contributors:
-+#     alexis timic
-+#     mguillaume
-+#
+#!/bin/bash -e
 
-
-
-#!/bin/bash
+#
+#
+# (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+#     alexis timic
+#     mguillaume
+#
 
 dir=$(dirname $0)
 ${dir}/pull_images
 
-#for i in 1; do
-#    slaveup=$(docker ps -f "status=running" -f "name=priv-nomaven-$i" | grep -v CONTAINER)
-#    if [ -z "$slaveup" ]; then
-#        docker run --restart=always --privileged -d -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h priv-nomaven-$i --name=priv-nomaven-$i -p 320$i:22 -t dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
-#    fi
-#done
 
 # priv slave for QA
 for i in 1 2; do
@@ -40,11 +32,6 @@ for i in 1 2; do
         docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h privovh01-$i --name=privovh01-$i -p 330$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
     fi
 done
-
-#staticprivup=$(docker ps -f "status=running" -f "name=staticpriv" | grep -v CONTAINER)
-#if [ -z "$staticprivup" ]; then
-#    docker run --restart=always --privileged -d --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h staticpriv --name=staticpriv -p 3301:22 -t dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
-#fi
 
 # priv slave for QA2
 for i in 1; do
