14bd6bffebf77c6c62d8f19926f72ccb7df1c1f9
Julien Carsique <jcarsique@nuxeo.com>
2018-07-16 16:42:29 +0200

    NXBT-1513: code review
    wip filepath master
    wip fix working dir
    fix load path
    wip missing scm
    fix })
    fix staticLabels
    remove -tt
    restrict on available slaves
    fix slave names
    only STATIC slaves
    no user confirm on QA2

--- qa-ovh-maintenance/qa-ovh01/start_remote_priv.sh
+++ qa-ovh-maintenance/qa-ovh01/start_remote_priv.sh
@@ -17,31 +17,19 @@
 # Contributors:
 #     alexis timic
 #
+# Start remote private slaves on qa-ovh01
 #
 
-#for i in 1; do
-#    slaveup=$(docker ps -f "status=running" -f "name=priv-nomaven-$i" | grep -v CONTAINER)
-#    if [[ -z "$slaveup" ]]; then
-#        docker run --restart=always --privileged -d -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h priv-nomaven-$i --name=priv-nomaven-$i -p 320$i:22 -t dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
-#    fi
-#done
-
-# priv slave for QA
-slave=$1
-
+# Reserved for QA
 for i in 1 2; do
-    slaveup=$(docker ps -f "status=running" -f "name=privovh01-$i" --format "{{.ID}}")
+    slaveup=$(docker ps -f "status=running" -f "name=priv-01-$i" --format "{{.ID}}")
     if [ -z "$slaveup" ]; then
-        docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h privovh01-$i --name=privovh01-$i -p 330$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
+        docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109
+--add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h priv-01-$i --name=priv-01-$i -p 33${i}0:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
     fi
 done
 
-#staticprivup=$(docker ps -f "status=running" -f "name=staticpriv" | grep -v CONTAINER)
-#if [[ -z "$staticprivup" ]]; then
-#    docker run --restart=always --privileged -d --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h staticpriv --name=staticpriv -p 3301:22 -t dockerpriv.nuxeo.com:443/nuxeo/jenkins-slave
-#fi
-
-# priv slave for QA2
+# Reserved for QA2
 for i in 1; do
     slaveup=$(docker ps -f "status=running" -f "name=priv2-01-$i" --format "{{.ID}}")
     if [ -z "$slaveup" ]; then
@@ -49,8 +37,8 @@ for i in 1; do
     fi
 done
 
-itslaveprivup=$(docker ps -f "status=running" -f "name=itslavepriv$" --format "{{.ID}}")
-if [ -z "$itslaveprivup" ]; then
-    docker run --restart=always --privileged -d --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace:/opt/jenkins/workspace:rw -h itslavepriv --name=itslavepriv -p 3401:22 -t dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv
+slaveup=$(docker ps -f "status=running" -f "name=itslavepriv01$" --format "{{.ID}}")
+if [ -z "$slaveup" ]; then
+    docker run --restart=always --privileged -d --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace:/opt/jenkins/workspace:rw -h itslavepriv01 --name=itslavepriv01 -p 3401:22 -t dockerpriv.nuxeo.com:443/nuxeo/jenkins-itpriv
 fi
 exit 0
--- qa-ovh-maintenance/qa-ovh02/start_remote_priv.sh
+++ qa-ovh-maintenance/qa-ovh02/start_remote_priv.sh
@@ -17,44 +17,36 @@
 # Contributors:
 #     alexis timic
 #
+# Start remote private slaves on qa-ovh02
 #
 
-
-slave=$1
-
-# priv slave for QA
+# Reserved for QA
 for i in 1 2; do
-    slaveup=$(docker ps -f "status=running" -f "name=privovh02-$i" --format "{{.NAMES}}")
+    slaveup=$(docker ps -f "status=running" -f "name=priv-02-$i" --format "{{.NAMES}}")
     if [ -z "$slaveup" ]; then
-        docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h privovh02-$i --name=privovh02-$i -p 330$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
+        docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h priv-02-$i --name=priv-02-$i -p 330$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
     fi
 done
 
-# priv slave for QA2
+# Reserved for QA2
 for i in 1; do
     slaveup=$(docker ps -f "status=running" -f "name=priv2-02-$i" --format "{{.ID}}")
     if [ -z "$slaveup" ]; then
         docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h priv2-02-$i --name=priv2-02-$i -p 440$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv
     fi
 done
-
-# static priv slave 7.10 for QA2
 for i in 1; do
     slaveup=$(docker ps -f "status=running" -f "name=slavepriv2-710-$i" --format "{{.ID}}")
     if [ -z "$slaveup" ]; then
         docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h slavepriv2-710-$i --name=slavepriv2-710-$i -p 550$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv-7.10
     fi
 done
-
-# static priv slave 8.10 for QA2
 for i in 1; do
     slaveup=$(docker ps -f "status=running" -f "name=slavepriv2-810-$i" --format "{{.ID}}")
     if [ -z "$slaveup" ]; then
         docker run --privileged -d --restart=always --add-host mavenpriv.in.nuxeo.com:176.31.235.109 --add-host mavenin.nuxeo.com:176.31.239.50 -v /var/run/docker.sock:/var/run/docker.sock:rw -v /opt/jenkins/workspace-priv:/opt/jenkins/workspace:rw -h slavepriv2-810-$i --name=slavepriv2-810-$i -p 560$i:22 -t -e NX_DB_HOST=127.0.0.1 -e NX_MONGODB_SERVER=127.0.0.1 dockerpriv.nuxeo.com:443/nuxeo/jenkins-slavepriv-8.10
     fi
 done
-
-# static priv slave 9.10 for QA2
 for i in 1; do
     slaveup=$(docker ps -f "status=running" -f "name=slavepriv2-910-$i" --format "{{.ID}}")
     if [ -z "$slaveup" ]; then
