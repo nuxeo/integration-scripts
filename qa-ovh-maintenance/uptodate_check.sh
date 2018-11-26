65d655176736193da49775c77a76f173205c6356
alexistimic <atimic@nuxeo.com>
2018-11-26 16:24:25 +0100

    NXBT-2619: Check if updated slave

---
+++ qa-ovh-maintenance/uptodate_check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+children_image_id=$(docker inspect static01 --format '{{.Image}}' | awk -F':' '{print substr($2,1,12)}')
+echo ${children_image_id}
+if docker images |grep ${children_image_id} |awk -F' ' '{print $2}'| grep '<none>' ; then
+        echo "success"
+fi
