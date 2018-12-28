eeeb72fd84d779dbe1546be9b763f9a7c72839ab
alexistimic <atimic@nuxeo.com>
2018-12-28 14:49:55 +0100

    NXBT-2619: source script

--- qa-ovh-maintenance/common/swarm_check.sh
+++ qa-ovh-maintenance/common/swarm_check.sh
@@ -5,7 +5,7 @@ SLAVE_ID=$(docker -H tcp://swarm-qa.nuxeo.org:4000 ps -f "status=running" -f "na
 
 if [ -n "$SLAVE_ID" ]; then
 
-    SLAVE_HOST=$(docker -H tcp://swarm-qa.nuxeo.org:4000 ps -f "status=running" -f "name=${SLAVE_NAME}" --format "{{.Names}}" |cut -d / -f 1)
+    SLAVE_HOST=$(docker -H tcp://swarm-qa.nuxeo.org:4000 ps -f "status=running" -f "name=${SLAVE_NAME}" --format "{{.Names}}" | cut -d '/' -f 1)
     SLAVE_IMAGE_ID=$(docker -H ${SLAVE_HOST}.nuxeo.com:4243 inspect ${SLAVE_NAME} --format '{{.Image}}' | awk -F':' '{print substr($2,1,12)}')
     if docker -H $SLAVE_HOST.nuxeo.com:4243 images |grep ${SLAVE_IMAGE_ID} |awk -F' ' '{print $2}'|grep '<none>'; then
         echo "${SLAVE_NAME} is outdated"
