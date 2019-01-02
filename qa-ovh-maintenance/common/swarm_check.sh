fbfefb4118a8fd277c5f2674321d71eea0361f94
alexistimic <atimic@nuxeo.com>
2019-01-02 17:24:12 +0100

    NXBT-2619: return code

--- qa-ovh-maintenance/common/swarm_check.sh
+++ qa-ovh-maintenance/common/swarm_check.sh
@@ -4,10 +4,10 @@ function check_update()
 {
      if docker -H $SLAVE_HOST.nuxeo.com:4243 images |grep ${SLAVE_IMAGE_ID} |awk -F' ' '{print $2}'|grep '<none>'; then
         echo "${SLAVE_NAME} is outdated"
-        return $(echo 1)
+        retval=1
     else
         echo "${SLAVE_NAME} is up to date"
-        return $(echo 0)
+        retval=0
     fi
 
 }
@@ -19,5 +19,6 @@ if [ -n "$SLAVE_ID" ]; then
 
     SLAVE_HOST=$(docker -H tcp://swarm-qa.nuxeo.org:4000 ps -f "status=running" -f "name=${SLAVE_NAME}" --format "{{.Names}}" | cut -d '/' -f 1)
     SLAVE_IMAGE_ID=$(docker -H ${SLAVE_HOST}.nuxeo.com:4243 inspect ${SLAVE_NAME} --format '{{.Image}}' | awk -F':' '{print substr($2,1,12)}')
+    retval=0
     check_update
 fi
