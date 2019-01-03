4ce8f3dd8757fd4d523055c2734b77b776d5352f
alexistimic <atimic@nuxeo.com>
2019-01-03 14:24:40 +0100

    NXBT-2619: wip

--- qa-ovh-maintenance/update_static_slaves.groovy
+++ qa-ovh-maintenance/update_static_slaves.groovy
@@ -130,7 +130,7 @@ def update_static_slaves(boolean doConfirm=false) {
             """
           }
           // trigger this job again if we have set slaves offline
-          if (onlineBusySlaves.size() > 0) {
+          if (onlineBusySlaves != null) {
             build job: 'update_static_slaves', propagate: false, quietPeriod: 3600
           }
         }
